e64537d2ebc3c7508876ffa0a00b7b1a460a416e
Luc MICHEL <luc.michel@git.antfield.fr>
2016-02-22 19:03:28 +0100

    test: improve exception handling and reporting and show on signal child termination

--- librabbits/test/entry.cc
+++ librabbits/test/entry.cc
@@ -5,15 +5,18 @@
 #include "rabbits/platform/description.h"
 #include "rabbits/dynloader/dynloader.h"
 
+#include <cstring>
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+using std::string;
 
 std::vector<TestFactory*> * TestFactory::m_insts = NULL;
 
-ComponentBase * Test::create_component_by_name(const std::string name,
-                                               const std::string yml_params)
+ComponentBase * Test::create_component_by_name(const string name,
+                                               const string yml_params)
 {
     ComponentManager &cm = ComponentManager::get();
     ComponentFactory *cf = cm.find_by_name(name);
@@ -30,27 +33,69 @@ ComponentBase * Test::create_component_by_name(const std::string name,
 
 static int do_test(TestFactory *tf)
 {
+
+    Test *t = NULL;
+    bool tests_passed;
+
     try {
-        Test *t = tf->create();
-        bool tests_passed;
-
-        sc_core::sc_start();
-
-        tests_passed = t->tests_passed();
-        delete t;
-
-        return (tests_passed) ? 0 : 1;
-
+        t = tf->create();
     } catch (TestFailureException e) {
         ERR_STREAM(tf->get_name() << ": Failed during elaboration: " << e.what_without_bt() << "\n");
         return 1;
-    } catch (std::runtime_error e) {
+    } catch (sc_core::sc_report e) {
+        ERR_STREAM(tf->get_name() << ": SystemC report during elaboration.\n" << e.what() << "\n");
+        return 1;
+    } catch (std::exception e) {
         ERR_STREAM(tf->get_name() << ": Unknown exception during elaboration.\n" << e.what() << "\n");
         return 1;
     } catch (...) {
         ERR_STREAM(tf->get_name() << ": Unknown exception during elaboration.\n");
         return 1;
     }
+
+    try {
+        sc_core::sc_start();
+    } catch (TestFailureException e) {
+        ERR_STREAM(tf->get_name() << ": Failed during test: " << e.what_without_bt() << "\n");
+        return 1;
+    } catch (sc_core::sc_report e) {
+        ERR_STREAM(tf->get_name() << ": SystemC report during test.\n" << e.what() << "\n");
+        return 1;
+    } catch (std::exception e) {
+        ERR_STREAM(tf->get_name() << ": Unknown exception during test.\n" << e.what() << "\n");
+        return 1;
+    } catch (...) {
+        ERR_STREAM(tf->get_name() << ": Unknown exception during test.\n");
+        return 1;
+    }
+
+    tests_passed = t->tests_passed();
+    delete t;
+
+    return (tests_passed) ? 0 : 1;
+}
+
+static int report_result(const string &name, int status)
+{
+    string res;
+    int ret = 0;
+
+    if (WIFEXITED(status)) {
+        if (WEXITSTATUS(status)) {
+            res = "FAIL";
+            ret = 1;
+        } else {
+            res = "OK";
+            ret = 0;
+        }
+    } else if (WIFSIGNALED(status)) {
+        res = "FAIL (" + string(strsignal(WTERMSIG(status))) + ")";
+        ret = 1;
+    }
+
+    INF_STREAM(name << "\r\t\t\t\t\t\t" << res << "\n");
+
+    return ret;
 }
 
 int sc_main(int argc, char *argv[])
@@ -60,7 +105,7 @@ int sc_main(int argc, char *argv[])
     TestFactory::const_iterator it;
     int result = 0;
 
-    if ((argc == 2) && (std::string(argv[1]) == "-d")) {
+    if ((argc == 2) && (string(argv[1]) == "-d")) {
         Logger::get().set_log_level(LogLevel::DEBUG);
     } else {
         Logger::get().set_log_level(LogLevel::INFO);
@@ -73,12 +118,11 @@ int sc_main(int argc, char *argv[])
 
     dyn_loader.search_and_load_rabbits_dynlibs();
     ComponentManager::get(); /* Force instantiation of ComponentManager
-                                and registration of components */
+                                and registration of components before forking */
 
     for (it = TestFactory::begin(); it != TestFactory::end(); it++) {
         int pid = fork();
         int status;
-        int test_result;
 
         if (pid == -1) {
             perror("fork");
@@ -91,15 +135,7 @@ int sc_main(int argc, char *argv[])
 
         wait(&status);
 
-        if (WIFEXITED(status)) {
-            test_result = WEXITSTATUS(status);
-        } else {
-            test_result = 1;
-        }
-
-        INF_STREAM((*it)->get_name() << "\r\t\t\t\t\t\t" << ((test_result == 0) ? "OK" : "FAIL") << "\n");
-
-        result |= test_result;
+        result |= report_result((*it)->get_name(), status);
     }
 
     return result;
