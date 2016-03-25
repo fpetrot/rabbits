21b7c8e6e27d7e1889a30bee2860b88c869db9f9
Luc MICHEL <luc.michel@git.antfield.fr>
2016-03-25 18:54:06 +0100

    doc: bus.h documentation

--- include/rabbits/component/bus.h
+++ include/rabbits/component/bus.h
@@ -17,6 +17,10 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+/**
+ * @file Bus and Bus interfaces class declaration.
+ */
+
 #ifndef _COMPONENTS_RASPBERRY_BUS_H
 #define _COMPONENTS_RASPBERRY_BUS_H
 
@@ -26,6 +30,13 @@
 #include "rabbits/component/component.h"
 #include "rabbits/rabbits_exception.h"
 
+/**
+ * @brief Bus component.
+ *
+ * This class represents a bus component. A bus component can connect Masters
+ * and Slaves together through BusMasterIface and BusSlaveIface interfaces, and
+ * forward TLM requests.
+ */
 class Bus : public Component, public BusIface {
 public:
     Bus(sc_core::sc_module_name name, ComponentParameters& params) :
@@ -33,6 +44,12 @@ public:
     virtual ~Bus() {}
 };
 
+
+/**
+ * @brief Wrong bus size exception.
+ *
+ * Exception thrown when two components of different bus sizes are connected together
+ */
 class WrongBusSizeException : public RabbitsException {
 protected:
     std::string build_what(unsigned int bits) {
@@ -49,10 +66,26 @@ public:
 template <unsigned int BUSWIDTH = 32>
 class BusSlaveIface;
 
+
+/**
+ * @brief Slave bus interface, base class.
+ *
+ * Base class of the slave bus interface. This class is bus size agnostic and a
+ * runtime check is performed to ensure bus size compatibility.
+ */
 class BusSlaveIfaceBase : public tlm::tlm_bw_transport_if<> {
 public:
     virtual ~BusSlaveIfaceBase() {}
 
+
+    /**
+     * @brief Get the target TLM socket associated to the interface.
+     *
+     * @tparam BUSWIDTH Bus size of the socket.
+     *
+     * @return the TLM socket associated to the interface.
+     * @throw WrongBusSizeException if BUSWIDTH does not match the socket bus size.
+     */
     template <unsigned int BUSWIDTH>
     tlm::tlm_target_socket<BUSWIDTH>& get_socket() {
         BusSlaveIface<BUSWIDTH> *bus = dynamic_cast<BusSlaveIface<BUSWIDTH>* >(this);
@@ -63,6 +96,14 @@ public:
     }
 };
 
+/**
+ * @brief Slave bus interface.
+ *
+ * Slave bus interface with fixed bus size. This class encapsulate the TLM
+ * target socket and provide all the methods a target socket provides.
+ *
+ * @tparam BUSWIDTH bus size of the interface.
+ */
 template <unsigned int BUSWIDTH>
 class BusSlaveIface : public BusSlaveIfaceBase {
 protected:
@@ -75,7 +116,26 @@ protected:
     }
 
 public:
+    /**
+     * @brief Construct a slave interface for a given slave.
+     *
+     * When using this constructor, the slave interface instance handles the TLM target socket itself.
+     * It creates and binds it to the slave on first use (ie. when connected to a bus).
+     *
+     * @param slave The Slave associated to this SlaveIface.
+     */
     BusSlaveIface(SlaveIface &slave) : m_socket(NULL), m_free_socket(false), m_slave(&slave) {}
+
+
+    /**
+     * @brief Construct a slave interface for a given TLM socket.
+     *
+     * When using this constructor, the slave interface assumes that the TLM
+     * target socket is fully valid and bound, and does not handle its
+     * lifetime.
+     *
+     * @param s The TLM target socket associated to this SlaveIface.
+     */
     BusSlaveIface(tlm::tlm_target_socket<BUSWIDTH> &s) : m_socket(&s), m_free_socket(false), m_slave(NULL) {}
 
     virtual ~BusSlaveIface() {
@@ -84,6 +144,17 @@ public:
         }
     }
 
+    /**
+     * @brief TLM Non-blocking transport backward.
+     *
+     * See TLM2.0 documentation.
+     *
+     * @param trans TLM payload.
+     * @param phase TLM phase.
+     * @param t elapsed time.
+     *
+     * @return TLM transport status
+     */
     tlm::tlm_sync_enum nb_transport_bw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& t) {
         if (m_socket == NULL) {
             abort();
@@ -92,6 +163,14 @@ public:
         return (*m_socket)->nb_transport_bw(trans, phase, t);
     }
 
+    /**
+     * @brief TLM DMI invalidation
+     *
+     * See TLM2.0 documentation
+     *
+     * @param start_range Start address
+     * @param end_range End address
+     */
     void invalidate_direct_mem_ptr(sc_dt::uint64 start_range, sc_dt::uint64 end_range) {
         if (m_socket == NULL) {
             abort();
@@ -100,6 +179,11 @@ public:
         (*m_socket)->invalidate_direct_mem_ptr(start_range, end_range);
     }
 
+    /**
+     * @brief Get the TLM socket associated to the interface
+     *
+     * @return the TLM target socket associated to the interface
+     */
     tlm::tlm_target_socket<BUSWIDTH>& get_socket() {
         if (m_socket == NULL) {
             m_socket = new tlm::tlm_target_socket<BUSWIDTH>;
@@ -107,7 +191,7 @@ public:
             m_free_socket = true;
         }
 
-        return *m_socket; 
+        return *m_socket;
     }
 };
 
@@ -115,10 +199,24 @@ public:
 template <unsigned int BUSWIDTH = 32>
 class BusMasterIface;
 
+/**
+ * @brief Master bus interface, base class.
+ *
+ * Base class of the master bus interface. This class is bus size agnostic and a
+ * runtime check is performed to ensure bus size compatibility.
+ */
 class BusMasterIfaceBase : public tlm::tlm_fw_transport_if<> {
 public:
     virtual ~BusMasterIfaceBase() {}
 
+    /**
+     * @brief Get the initiator TLM socket associated to the interface.
+     *
+     * @tparam BUSWIDTH Bus size of the socket.
+     *
+     * @return the TLM socket associated to the interface.
+     * @throw WrongBusSizeException if BUSWIDTH does not match the socket bus size.
+     */
     template <unsigned int BUSWIDTH>
     tlm::tlm_initiator_socket<BUSWIDTH>& get_socket() {
         BusMasterIface<BUSWIDTH> *bus = dynamic_cast<BusMasterIface<BUSWIDTH>* >(this);
@@ -129,6 +227,14 @@ public:
     }
 };
 
+/**
+ * @brief Master bus interface.
+ *
+ * Master bus interface with fixed bus size. This class encapsulate the TLM
+ * initiator socket and provide all the methods a initiator socket provides.
+ *
+ * @tparam BUSWIDTH bus size of the interface.
+ */
 template <unsigned int BUSWIDTH>
 class BusMasterIface : public BusMasterIfaceBase {
 protected:
@@ -137,7 +243,25 @@ protected:
     MasterIface *m_master;
 
 public:
+    /**
+     * @brief Construct a master interface for a given master.
+     *
+     * When using this constructor, the master interface instance handles the TLM initiator socket itself.
+     * It creates and binds it to the master on first use (ie. when connected to a bus).
+     *
+     * @param master The Master associated to this MasterIface.
+     */
     BusMasterIface(MasterIface &master) : m_socket(NULL), m_free_socket(false), m_master(&master) {}
+
+    /**
+     * @brief Construct a master interface for a given TLM socket.
+     *
+     * When using this constructor, the master interface assumes that the TLM
+     * initiator socket is fully valid and bound, and does not handle its
+     * lifetime.
+     *
+     * @param s The TLM initiator socket associated to this MasterIface.
+     */
     BusMasterIface(tlm::tlm_initiator_socket<BUSWIDTH> &m) : m_socket(&m), m_free_socket(false), m_master(NULL) {}
 
     virtual ~BusMasterIface() {
@@ -163,6 +287,11 @@ public:
         return (*m_socket)->nb_transport_fw(trans, phase, t);
     }
 
+    /**
+     * @brief Get the TLM socket associated to the interface
+     *
+     * @return the TLM initiator socket associated to the interface
+     */
     tlm::tlm_initiator_socket<BUSWIDTH>& get_socket() {
         if (m_socket == NULL) {
             m_socket = new tlm::tlm_initiator_socket<BUSWIDTH>;
