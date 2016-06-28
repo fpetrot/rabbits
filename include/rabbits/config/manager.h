fcffb1118f9ccbfd546d41f23be14dc3daf8ef1b
Luc MICHEL <luc.michel@git.antfield.fr>
2016-06-28 19:36:55 +0200

    Now configuration manager system.
    This system handles Rabbits configuration by reading configuration files
    and the command line, and extracting :
      - The platform list
      - The global options
    It allows for complex melting of multiple configuration files together
    to describe platforms and other configuration items.

---
+++ include/rabbits/config/manager.h
@@ -0,0 +1,94 @@
+/*
+ *  This file is part of Rabbits
+ *  Copyright (C) 2015  Clement Deschamps and Luc Michel
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _RABBITS_CONFIG_MANAGER_H
+#define _RABBITS_CONFIG_MANAGER_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "rabbits/component/parameters.h"
+#include "rabbits/platform/description.h"
+
+namespace boost {
+    namespace filesystem {
+        class path;
+    }
+}
+
+class ConfigManager {
+public:
+    typedef std::map<std::string, PlatformDescription> Platforms;
+    typedef std::map<std::string, ParameterBase*> ParamAliases;
+
+private:
+    PlatformDescription m_root_descr;
+    PlatformDescription m_cmdline_descr;
+    PlatformDescription m_config_file_descr;
+
+    Platforms m_platforms;
+
+    ComponentParameters m_global_params;
+
+    std::vector<std::string> m_dynlibs_to_load;
+    std::set<std::string> m_loaded_config_files;
+    ParamAliases m_aliases;
+
+    std::string m_platform_basename;
+
+    bool m_is_recomputing_config = false;
+
+    void apply_description(PlatformDescription &d);
+    void parse_basename(const char *arg0);
+    void load_config_directory(boost::filesystem::path p);
+    void load_config_from_description();
+
+    void compute_platform(const std::string &name, const PlatformDescription &p);
+    void recompute_platforms();
+    void apply_aliases();
+    void recompute_config();
+
+    void build_cmdline_unaries(std::set<std::string> &unaries) const;
+
+public:
+    ConfigManager();
+    virtual ~ConfigManager();
+
+    void add_cmdline(int argc, const char * const argv[]);
+    void add_yml_file(const std::string &filename);
+    void add_yml(const std::string &yml_descr);
+    void add_config_file(const std::string &path);
+
+    bool config_file_is_loaded(const std::string &path) const;
+
+    void add_param_alias(const std::string &alias_key, ParameterBase &dest);
+    void add_global_param(const std::string &key, const ParameterBase &param);
+    ComponentParameters & get_global_params();
+    
+    void get_dynlibs_to_load(std::vector<std::string> &dynlibs) const;
+
+    bool platform_exists(const std::string &name) const { return m_platforms.find(name) != m_platforms.end(); }
+    PlatformDescription get_platform(const std::string &name) const;
+    PlatformDescription apply_platform(const std::string &name);
+
+    PlatformDescription get_root_description() const { return m_root_descr; }
+};
+
+#endif
---
+++ librabbits/config/manager.cc
@@ -0,0 +1,313 @@
+/*
+ *  This file is part of Rabbits
+ *  Copyright (C) 2015  Clement Deschamps and Luc Michel
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "rabbits/config.h"
+#include "rabbits/config/manager.h"
+#include "rabbits/logger.h"
+
+#include <boost/filesystem.hpp>
+
+using std::set;
+using std::vector;
+using std::string;
+using namespace boost::filesystem;
+
+ConfigManager::ConfigManager()
+{
+    add_global_param("configuration-directory",
+                     Parameter<string>("Where to find configuration files",
+                                       RABBITS_DESCR_SEARCH_PATH));
+
+    add_global_param("selected-platform",
+                     Parameter<string>("The selected platform",
+                                       ""));
+}
+
+ConfigManager::~ConfigManager()
+{
+}
+
+void ConfigManager::apply_aliases()
+{
+    for (auto alias : m_aliases) {
+        const string &key = alias.first;
+        ParameterBase &param = *alias.second;
+        
+        if (m_root_descr[key].is_scalar()) {
+            param.set(m_root_descr[key]);
+        }
+    }
+}
+
+void ConfigManager::compute_platform(const string &name, const PlatformDescription &p)
+{
+    PlatformDescription platform = p;
+
+    if (platform["inherit"].is_scalar()) {
+        const string parent_name = platform["inherit"].as<string>();
+
+        if (!platform_exists(parent_name)) {
+            ERR_STREAM("Platform " << name << " inherits from unknown platform " << parent_name << "\n");
+            return;
+        }
+
+        DBG_STREAM("Platform " << name << " inherits from " << parent_name << "\n");
+
+        PlatformDescription parent = get_platform(parent_name);
+        
+        platform = platform.merge(parent);
+    }
+
+    m_platforms[name] = platform;
+}
+
+void ConfigManager::recompute_platforms()
+{
+    m_platforms.clear();
+
+    if (!m_root_descr["platforms"].is_map()) {
+        DBG_STREAM("No platform found.\n");
+        return;
+    }
+
+    for (auto &p : m_root_descr["platforms"]) {
+        DBG_STREAM("Found platform " << p.first << "\n");
+        compute_platform(p.first, p.second);
+    }
+}
+
+void ConfigManager::load_config_from_description()
+{
+    if (!m_root_descr.exists("config")) {
+        return;
+    }
+
+    PlatformDescription conf = m_root_descr["config"];
+    set<string> to_load;
+
+    if (conf.is_map()) {
+        for (auto c: conf) {
+            PlatformDescription & cc = c.second;
+            if (cc.is_scalar()) {
+                to_load.insert(cc.as<string>());
+            }
+        }
+    } else if (conf.is_scalar()) {
+        to_load.insert(conf.as<string>());
+    }
+
+    for (auto c: to_load) {
+        add_config_file(c);
+    }
+}
+
+void ConfigManager::recompute_config()
+{
+    m_root_descr = m_cmdline_descr.merge(m_config_file_descr);
+
+    load_config_from_description();
+
+    if (m_is_recomputing_config) {
+        return;
+    }
+
+    m_is_recomputing_config = true;
+
+    m_global_params.fill_from_description(m_root_descr["global"]);
+
+    if (!m_platform_basename.empty()) {
+        m_global_params["selected-platform"] = m_platform_basename;
+    }
+
+    apply_aliases();
+    recompute_platforms();
+
+    m_is_recomputing_config = false;
+}
+
+void ConfigManager::apply_description(PlatformDescription &d)
+{
+    m_config_file_descr = d.merge(m_config_file_descr);
+    recompute_config();
+}
+
+void ConfigManager::parse_basename(const char *arg0)
+{
+    const string basename = path(arg0).filename().string();
+
+    if (basename != RABBITS_APP_NAME) {
+        DBG_STREAM("Trying to deduce selected platform from basename\n");
+        const string prefix(RABBITS_DESCR_SYMLINK_PREFIX);
+
+        if (basename.find(prefix) != 0) {
+            DBG_STREAM("basename seems invalid. Giving up.\n");
+        } else {
+            m_platform_basename = basename.substr(prefix.size());
+        }
+    }
+}
+
+void ConfigManager::load_config_directory(path p)
+{
+    directory_iterator d;
+
+    DBG_STREAM("Loading configuration files from " << p << "\n");
+
+    if (!exists(p)) {
+        DBG_STREAM("Directory " << p << " not found.\n");
+        return;
+    }
+
+    if (!is_directory(p)) {
+        DBG_STREAM(p << " is not a directory.\n");
+        return;
+    }
+
+    for (d = directory_iterator(p); d != directory_iterator(); d++) {
+        directory_entry &dd = *d;
+
+        if (is_directory(dd)) {
+            load_config_directory(dd.path());
+        } else if (is_regular_file(dd)) {
+            add_config_file(dd.path().string());
+        } else {
+            DBG_STREAM("Skipping non-regular file " << dd.path() << "\n");
+        }
+
+    }
+}
+
+void ConfigManager::build_cmdline_unaries(set<string> &unaries) const
+{
+    /* We take every aliases that have boolean type */
+    for (const auto &pair : m_aliases) {
+        const string key = pair.first;
+        const ParameterBase &p = *pair.second;
+
+        if (p.is_convertible_to<bool>()) {
+            unaries.insert(key);
+        }
+    }
+}
+
+void ConfigManager::add_cmdline(int argc, const char * const argv[])
+{
+    set<string> unaries;
+
+    parse_basename(argv[0]);
+
+    build_cmdline_unaries(unaries);
+    m_cmdline_descr.parse_cmdline(argc, argv, unaries);
+
+    recompute_config();
+
+    path conf_dir(m_global_params["configuration-directory"].as<string>());
+    load_config_directory(conf_dir);
+}
+
+bool ConfigManager::config_file_is_loaded(const std::string &path) const
+{
+    return m_loaded_config_files.find(path) != m_loaded_config_files.end();
+}
+
+void ConfigManager::add_yml_file(const string &filename)
+{
+    PlatformDescription d;
+
+    DBG_STREAM("Loading YAML config file " << filename << "\n");
+
+    if (config_file_is_loaded(filename)) {
+        DBG_STREAM(filename << " is already loaded. Skipping.\n");
+        return;
+    }
+
+    try {
+        d.load_file_yaml(filename);
+    } catch (std::exception e) {
+        ERR_STREAM("Failed to load YAML config file " << filename << ": " << e.what() << "\n");
+        return;
+    }
+
+    m_loaded_config_files.insert(filename);
+
+    apply_description(d);
+}
+
+void ConfigManager::add_yml(const string &yml_descr)
+{
+    PlatformDescription d;
+
+    d.load_yaml(yml_descr);
+
+    apply_description(d);
+}
+
+void ConfigManager::add_config_file(const std::string &path_s)
+{
+    path p(path_s);
+    string ext = p.extension().string();
+
+    std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+
+    if ((ext == ".yml") || (ext == ".yaml")) {
+        add_yml_file(path_s);
+    } else {
+        DBG_STREAM("Ignoring unknown file " << p << "\n");
+    }
+}
+
+void ConfigManager::add_param_alias(const std::string &alias_key, ParameterBase &dest)
+{
+    m_aliases[alias_key] = &dest;
+}
+
+void ConfigManager::add_global_param(const string &key, const ParameterBase &param)
+{
+    m_global_params.add(key, param);
+}
+
+ComponentParameters & ConfigManager::get_global_params()
+{
+    return m_global_params;
+}
+
+void ConfigManager::get_dynlibs_to_load(vector<string> &dynlibs) const
+{
+    std::copy(m_dynlibs_to_load.begin(), m_dynlibs_to_load.end(), dynlibs.begin());
+}
+
+PlatformDescription ConfigManager::get_platform(const string &name) const
+{
+    if (platform_exists(name)) {
+        return m_platforms.at(name);
+    } else {
+        return PlatformDescription::INVALID_DESCRIPTION;
+    }
+}
+
+PlatformDescription ConfigManager::apply_platform(const string &name)
+{
+    if (platform_exists(name)) {
+        PlatformDescription p = get_platform(name);
+        apply_description(p);
+        return get_root_description();
+    } else {
+        return PlatformDescription::INVALID_DESCRIPTION;
+    }
+}
