bb048627f09d70cf2b96cdc1353e9a743cd5ee20
Luc MICHEL <luc.michel@git.antfield.fr>
2017-02-14 11:07:27 +0100

    cmake: Fix test payload generation.
    Use add_custom_command instead of file(generate to generate the payload.
    This way, cmake is able to compute the dependencies and generate the
    payload before compiling the test.

--- cmake/rabbits_components.cmake
+++ cmake/rabbits_components.cmake
@@ -106,13 +106,11 @@ function(rabbits_generate_tests n)
 
         set(_test_payload ${CMAKE_CURRENT_BINARY_DIR}/${n}_test_payload.cc)
 
-        if("${CMAKE_VERSION}" VERSION_GREATER 3.0.2)
-            file(GENERATE
-                OUTPUT ${_test_payload}
-                CONTENT "namespace test { const char * test_payload = \"$<TARGET_FILE:${_test_mod}>\"; };")
-        else()
-            file(WRITE ${_test_payload} "namespace test { const char * test_payload = \"$<TARGET_FILE:${_test_mod}>\"; };")
-        endif()
+        add_custom_command(
+            OUTPUT "${_test_payload}"
+            COMMAND ${CMAKE_COMMAND} -E echo
+                'namespace test { const char * test_payload = \"$<TARGET_FILE:${_test_mod}>\"\; }\;'
+                >"${_test_payload}")
 
         add_executable(${_test_name} ${_test_payload})
         target_link_libraries(${_test_name} ${SYSTEMC_LIBRARIES} ${RABBITS_TEST_LIBRARY})
